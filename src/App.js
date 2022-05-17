b69d5af58238314ce8098049d129cd8790d24351
Ally <el.ally741@gmail.com>
2022-05-17 10:12:16 +0200

    second push

--- src/App.js
+++ src/App.js
@@ -3,23 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img sr  c={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div>
-        the beggining
-      </div>
 
       <div className='navigation'>
         <div className='logo'>OpenMRS</div>
@@ -31,7 +14,166 @@ function App() {
           <div>&#x1F50E;</div>
         </div>
       </div>
+      <div className='all'>
+      <div>
+      <h2>Creating New Patient</h2>
+      <label className='fading'>Jump to</label><br></br>
+      <div>&#8627; Identifiers</div><br></br>
+      <div>&#8627; Personal Details</div>
+      <div>&#8627; Domicile Details</div>
+      <div>&#8627; Residence Details</div>
+      <div>&#8627; Other Details</div>                         
+      </div>
+      <div>
 
+
+      <div>
+        <h4>Search</h4>
+        <div className='background'>
+        <select name="selection" id="type">
+        <option value="Unknown">Unknown</option>
+        <option value="Patient">Patient</option>
+        <option value="Doctor">Doctor</option>
+        </select>
+        </div>
+      </div>
+
+
+      <div>
+      <h4>Identifiers</h4>
+      <label className='fading'>All fields are required unless the marked options</label><br></br>
+      <div className='background'><br></br>
+        <label>UPID</label><br></br>
+        <input></input><br></br>
+        <label>Primary case Id</label><br></br>
+        <input></input><br></br>
+        <label>NID</label><br></br>
+        <input></input><br></br>
+        <label>NIN</label><br></br>
+        <input></input><br></br>
+        <label>NID Application number</label><br></br>
+        <input></input><br></br>
+        <label>Passport</label><br></br>
+        <input></input><br></br>
+        
+        </div>
+      </div>
+
+
+      <div>
+      <h4>Person Details</h4>
+      <label className='fading'>All fields are required unless the marked options</label><br></br>
+      <div className='background'><br></br>
+      <label>Surname</label><br></br><br></br>
+        <input></input><br></br><br></br>
+        <label>Post-names</label><br></br><br></br>
+        <input></input><br></br>
+        <div className='dateAndSex'>
+        {/* <script>
+date = new Date();
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
+document.getElementById("current_date").innerHTML = month + "/" + day + "/" + year;
+</script> */}
+        </div>
+        <label className='fading'>Vital Status</label><br></br>
+        <select name="selection" id="type"><br></br>
+        <option value="Select">Select</option>
+        </select>
+        <label className='fading'>Marital Status</label><br></br>
+        <select name="selection" id="type"><br></br>
+        <option value="Select">Select</option>
+        </select>
+        <label className='fading'>Nationality</label><br></br>
+        <select name="selection" id="type"><br></br>
+        <option value="Select">Rwandan</option>
+        </select>   
+        <label>father name</label><br></br>
+        <input></input><br></br>
+        <label>mother name</label><br></br>
+        <input></input><br></br>
+        <label>spouse name</label><br></br>
+        <input></input><br></br>
+        </div>
+      </div>
+
+      <div>
+      <h4>Domicile details</h4>
+      <label className='fading'>All fields are required unless the marked options</label><br></br>
+      <div className='background'><br></br>
+      <label>Country</label><br></br>
+        <input></input><br></br>
+        <label>Province</label><br></br>
+        <input></input><br></br>
+        <label>District</label><br></br>
+        <input></input><br></br>
+        <label>Sector</label><br></br>
+        <input></input><br></br>
+        <label>Cell</label><br></br>
+        <input></input><br></br>
+        <label>Village</label><br></br>
+        <input></input><br></br>
+        </div>
+      </div>
+
+      <div>
+      <h4>Residence details</h4>
+      <label className='fading'>All fields are required unless the marked options</label><br></br>
+      <div className='background'><br></br>
+      <label>Country</label><br></br>
+      <select name="selection" id="ty<br></br>pe">
+        <option value="Select">Rwanda</option>
+        </select> 
+        <label>Province</label><br></br>
+        <select name="selection" id="typ<br></br>e">
+        <option value="Select">Select</option>
+        </select> 
+        <label>District</label><br></br>
+        <select name="selection" id="typ<br></br>e">
+        <option value="Select">Select</option>
+        </select> 
+        <label>Sector</label><br></br>
+        <select name="selection" id="t<br></br>ype">
+        <option value="Select">Select</option>
+        </select> 
+        <label>Cell</label><br></br>
+        <select name="selection" id="type">
+        <option value="Select">Select</option>
+        </select> 
+        <label>Village</label><br></br>
+        <select name="selection" id="ty<br></br>pe">
+        <option value="Select">Select</option>
+        </select> 
+        <label>Address</label><br></br>
+        <input></input><br></br>
+        </div>
+      </div>
+
+      <div>
+      <h4>Other details</h4>
+      <label className='fading'>All fields are required unless the marked options</label><br></br>
+      <div className='background'><br></br>
+      <label>Phone</label><br></br>
+        <input></input><br></br>
+        <label>Education Level</label><br></br>
+        <select name="selection" id="type"><br></br>
+        <option value="Select">Select</option>
+        </select> 
+        <label>Religion</label><br></br>
+        <input></input><br></br>
+        <label>Profession</label><br></br>
+        <select name="selection" id="type">
+        <option value="Select">Select</option>
+        </select> 
+        </div>
+      </div>
+      <div>
+        <div>Create patient</div>
+        <div>Clear</div>
+      </div>
+      </div>
+      </div>
     </div>
   );
 }
